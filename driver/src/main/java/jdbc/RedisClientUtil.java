1a5280279f707ece59cc93efab7da2d5fee9c290
Anna Rodionova <veirisa1997@gmail.com>
2022-08-25 21:29:59 +0200

    Encoding for composite commands

--- driver/src/main/java/jdbc/RedisClientUtil.java
+++ driver/src/main/java/jdbc/RedisClientUtil.java
@@ -1,5 +1,6 @@
 package jdbc;
 
+import jdbc.RedisQuery.CompositeCommand;
 import jdbc.resultset.RedisEmptyResultSet;
 import jdbc.resultset.RedisListResultSet;
 import jdbc.resultset.RedisMapResultSet;
@@ -22,6 +23,15 @@ public class RedisClientUtil {
     private static final Map<String, Protocol.Command> COMMANDS =
             Arrays.stream(Protocol.Command.values()).collect(Collectors.toMap(Enum::name, v -> v));
 
+    private static final Map<String, Protocol.Keyword> KEYWORDS =
+            Arrays.stream(Protocol.Keyword.values()).collect(Collectors.toMap(Enum::name, v -> v));
+
+    private static final Set<Protocol.Command> COMMANDS_WITH_KEYWORDS = Set.of(
+            Protocol.Command.ACL, Protocol.Command.CLIENT, Protocol.Command.CLUSTER, Protocol.Command.CONFIG,
+            Protocol.Command.MEMORY, Protocol.Command.MODULE, Protocol.Command.OBJECT, Protocol.Command.PUBSUB,
+            Protocol.Command.SCRIPT, Protocol.Command.SLOWLOG, Protocol.Command.XGROUP, Protocol.Command.XINFO
+    );
+
     private static Protocol.Command parseCommand(String command) throws SQLException {
         Protocol.Command redisCommand = COMMANDS.get(command.toUpperCase(Locale.ENGLISH));
         if (redisCommand == null)
@@ -29,27 +39,34 @@ public class RedisClientUtil {
         return redisCommand;
     }
 
+    private static Protocol.Keyword parseKeyword(Protocol.Command redisCommand, String keyword) {
+        if (!COMMANDS_WITH_KEYWORDS.contains(redisCommand)) return null;
+        return KEYWORDS.get(keyword.toUpperCase(Locale.ENGLISH));
+    }
+
     public static RedisQuery parseQuery(String sql) throws SQLException {
         if (sql == null) throw new SQLException("Empty query.");
         String[] tokens = sql.split("\\s+");
         if (tokens.length == 0) throw new SQLException("Empty query.");
         Protocol.Command redisCommand = parseCommand(tokens[0]);
+        Protocol.Keyword redisKeyword = tokens.length > 1 ?  parseKeyword(redisCommand ,tokens[1]) : null;
         String[] params = Arrays.stream(tokens).skip(1).toArray(String[]::new);
-        return new RedisQuery(redisCommand, params);
+        return new RedisQuery(redisCommand, redisKeyword, params);
     }
 
 
-    private static final Map<Protocol.Command, Builder<?>> RESULT_BUILDERS = new HashMap<>() {{
-        put(Protocol.Command.HGETALL, BuilderFactory.STRING_MAP);
+    private static final Map<CompositeCommand, Builder<?>> RESULT_BUILDERS = new HashMap<>() {{
+        put(CompositeCommand.create(Protocol.Command.HGETALL), BuilderFactory.STRING_MAP);
+        put(CompositeCommand.create(Protocol.Command.CONFIG, Protocol.Keyword.GET), BuilderFactory.STRING_MAP);
     }};
 
-    private static Builder<?> getResultBuilder(Protocol.Command command) {
+    private static Builder<?> getResultBuilder(CompositeCommand command) {
         return RESULT_BUILDERS.getOrDefault(command, BuilderFactory.ENCODED_OBJECT);
     }
 
     private static Object encodeResult(RedisQuery query, Object result) {
         if (result == null) return null;
-        Builder<?> resultBuilder = getResultBuilder(query.getCommand());
+        Builder<?> resultBuilder = getResultBuilder(query.getCompositeCommand());
         return resultBuilder.build(result);
     }
 
